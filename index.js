2fc8b6d5bd54f1c0f25bd1c3a8dc005d12c82523
dlively1 <dan.lively@jazzhr.com>
2019-05-31 22:19:23 -0400

    js-lint Add jshint and do some basic cleanup

--- index.js
+++ index.js
@@ -12,15 +12,12 @@
  *  and copy each metadata item into a matching folder.
  *  Also if any deletes occurred it will create a file at ./deploy/featureBranch/destructive/destructiveChanges.xml
  */
-
-var program = require('commander');
-var util = require('util'),
-    spawnSync = require('child_process').spawnSync,
-    packageWriter = require('./lib/metaUtils').packageWriter,
-    buildPackageDir = require('./lib/metaUtils').buildPackageDir,
-    copyFiles = require('./lib/metaUtils').copyFiles,
-    packageVersion = require('./package.json').version;
-
+const program = require('commander');
+const spawnSync = require('child_process').spawnSync;
+const packageWriter = require('./lib/metaUtils').packageWriter;
+const buildPackageDir = require('./lib/metaUtils').buildPackageDir;
+const copyFiles = require('./lib/metaUtils').copyFiles;
+const packageVersion = require('./package.json').version;
 
 program
     .arguments('<compare> <branch> [target]')
@@ -35,10 +32,7 @@ program
             process.exit(1);
         }
 
-        var dryrun = false;
-        if (program.dryrun) {
-            dryrun = true;
-        }
+        const dryrun = program.dryrun;
 
         if (!dryrun && !target) {
             console.error('target required when not dry-run');
@@ -46,29 +40,28 @@ program
             process.exit(1);
         }
 
-        var currentDir = process.cwd();
+        const currentDir = process.cwd();
         const gitDiff = spawnSync('git', ['--no-pager', 'diff', '--name-status', compare, branch]);
-        var gitDiffStdOut = gitDiff.stdout.toString('utf8');
-        var gitDiffStdErr = gitDiff.stderr.toString('utf8');
+        const gitDiffStdOut = gitDiff.stdout.toString();
+        const gitDiffStdErr = gitDiff.stderr.toString();
 
         if (gitDiffStdErr) {
             console.error('An error has occurred: %s', gitDiffStdErr);
             process.exit(1);
         }
 
-        var fileListForCopy = [],
-            fileList = [];
+        let fileListForCopy = [];
 
         //defines the different member types
-        var metaBag = {};
-        var metaBagDestructive = {};
-        var deletesHaveOccurred = false;
+        const metaBag = {};
+        const metaBagDestructive = {};
+        let deletesHaveOccurred = false;
 
-        fileList = gitDiffStdOut.split('\n');
-        fileList.forEach(function (fileName, index) {
+        const fileList = gitDiffStdOut.split('\n');
+        fileList.forEach(function (fileName) {
 
             // get the git operation
-            var operation = fileName.slice(0,1);
+            const operation = fileName.slice(0,1);
             // remove the operation and spaces from fileName
             fileName = fileName.slice(1).trim();
 
@@ -80,7 +73,7 @@ program
                     return;
                 }
 
-                var parts = fileName.split('/');
+                const parts = fileName.split('/');
                 // Check for invalid fileName, likely due to data stream exceeding buffer size resulting in incomplete string
                 // TODO: need a way to ensure that full fileNames are processed - increase buffer size??
                 if (parts[2] === undefined) {
@@ -88,7 +81,7 @@ program
                     process.exit(1);
                 }
 
-                var meta;
+                let meta;
 
                 if (parts.length === 4) {
                     // Processing metadata with nested folders e.g. emails, documents, reports
@@ -129,10 +122,10 @@ program
             }
         });
 
-        //build package file content
-        var packageXML = packageWriter(metaBag, program.pversion);
-        //build destructiveChanges file content
-        var destructiveXML = packageWriter(metaBagDestructive, program.pversion);
+        // build package file content
+        const packageXML = packageWriter(metaBag, program.pversion);
+        // build destructiveChanges file content
+        const destructiveXML = packageWriter(metaBagDestructive, program.pversion);
         if (dryrun) {
             console.log('\npackage.xml\n');
             console.log(packageXML);
@@ -144,14 +137,12 @@ program
         console.log('Building in directory %s', target);
 
         buildPackageDir(target, branch, metaBag, packageXML, false, (err, buildDir) => {
-
             if (err) {
                 return console.error(err);
             }
 
             copyFiles(currentDir, buildDir, fileListForCopy);
             console.log('Successfully created package.xml and files in %s',buildDir);
-
         });
 
         if (deletesHaveOccurred) {
